729140dc4670fbab7fcd4c02536aa98761699b09
andii0890 <greenlounge@hotmail.com>
2019-06-03 22:12:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-312x9c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-312x9c)
---
+++ index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+import Contador from "./Contador"
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    let nombre = "Uriel";
+    return (
+      <div>
+        <Contador />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
